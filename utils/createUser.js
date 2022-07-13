17a50add66a47566e9b083afac31e11ca6d600b3
mertje <mert34gunes@gmail.com>
2022-07-13 23:01:15 +0200

    write new created account to DB

--- utils/createUser.js
+++ utils/createUser.js
@@ -1,11 +1,19 @@
 import { auth } from "../firebase/appClient";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
 
 export default  function createUser(userName, password) {
   return createUserWithEmailAndPassword(auth, userName, password)
+  .then((user) => {
+    const db = getFirestore();
+    const cityRef = doc(db, 'users', user.user.uid);
+    setDoc(cityRef, { email: userName, userUID: user.user.uid, groups: [] }, { merge: true } );
+  })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     return errorCode
   });
 }
+
